b95023e25412826580cd812596ae5444211538b7
Adrian Almeida <adrianalmeida417@gmail.com>
2025-06-01 16:29:54 -0300

    chore: configurando banco

---
+++ lib/config/database_config.dart
@@ -0,0 +1,70 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseConfig {
+  
+  static Database? _db;
+  static bool _isSetupped = false;
+  static String _path = "";
+  static final String _dbname = "cadinho.db";
+
+  static void setup() async {
+  if (_isSetupped) return;
+
+    String dbPath = await getDatabasesPath();
+    _path = join(dbPath, _dbname);
+    
+    try {
+      await Directory(dirname(_path)).create(recursive: true);
+    } catch (_) {}
+    
+    _isSetupped = true;
+  }
+
+  static Future<Database> get() async {
+    if (_db != null) return _db!;
+  
+    setup();
+
+    _db = await openDatabase(
+      _path, 
+      version: 1, 
+      onConfigure: _onConfigure,
+      onCreate: _onCreate
+    );
+
+    return _db!;
+  }
+
+  static close() {
+    _db?.close();
+  }
+
+  static _onConfigure(Database db) async {
+    await db.execute("PRAGMA foreign_keys = ON");
+  }
+
+  static _onCreate(Database db, int version) async {
+    await db.execute("""CREATE TABLE Lista (
+      id        INTEGER     PRIMARY KEY, 
+      titulo    TEXT        CHECK(LENGTH(titulo) <= 100)                            NOT NULL UNIQUE,
+      mercado   TEXT        CHECK(LENGTH(mercado) <= 100)                           NOT NULL DEFAULT 'Não informado',
+      data      TEXT                                                                NOT NULL DEFAULT current_timestamp,
+      status    TEXT        CHECK(status IN('PENDENTE', 'EM CURSO', 'FINALIZADO'))  NOT NULL DEFAULT 'PENDENTE'
+    )""");
+   
+    await db.execute("""CREATE TABLE Item (
+      id            INTEGER PRIMARY KEY,
+      titulo        TEXT    CHECK(LENGTH(titulo) <= 100)                NOT NULL,
+      valor         REAL    CHECK(valor > 0 AND valor <= 500)           NOT NULL DEFAULT 1,
+      quantidade    INTEGER CHECK(quantidade > 0)                       NOT NULL DEFAULT 1,
+      promocional   REAL    CHECK(promocional > 0 AND promocional <= valor),
+      qt_promocao   INTEGER CHECK(qt_promocao > 1),
+      id_lista      INTEGER CHECK(id_lista > 0)                         NOT NULL,
+      FOREIGN KEY(id_lista) REFERENCES Lista(id) ON DELETE CASCADE
+    )""");
+  }
+}
